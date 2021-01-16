129946aaca5edc1f80a027452fd0f44e3293c22f
Mohamed Abdelnasser <mohamedabdelnasser@MacBook-Air.local>
2021-01-16 21:23:49 +0000

    add JavaScript

---
+++ main.js
@@ -0,0 +1,33 @@
+const form = document.querySelector(".form");
+const input = document.querySelectorAll(".input");
+
+form.addEventListener("submit", (e)=> {
+    e.preventDefault()
+    for(let i = 0; i < input.length ; i++){
+
+        const inputEl = input[i];
+        const inputParentEl = inputEl.parentElement;
+        const errorMsgEl = inputParentEl.querySelector(".error-msg");
+        function ValidateEmail(mail) {
+            return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail.value)
+            };
+
+            
+        if (inputEl.value ==""){
+            inputEl.classList.add("error");
+            errorMsgEl.style.display = "block";
+        }else if(inputEl.id =="email-address"){
+            if(!ValidateEmail(inputEl)){
+                inputEl.classList.add("error");
+                errorMsgEl.innerHTML ="You have entered an invalid email address!";
+                errorMsgEl.style.display = "block";
+            }else{
+                inputEl.classList.remove("error");
+                errorMsgEl.style.display = "none";
+            }
+        }else{
+            inputEl.classList.remove("error");
+            errorMsgEl.style.display = "none";
+        }
+    }
+});
